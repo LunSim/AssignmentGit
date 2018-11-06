ff8d069d93499dfde9ee0f05a44d9e30b91068c3
Lun Sim <lunsim168@gmail.com>
2018-11-06 13:48:51 +0700

    Add activity to koko Branch

---
+++ app/src/main/java/com/example/lunsim/assignmentgit/kokoActivity.java
@@ -0,0 +1,13 @@
+package com.example.lunsim.assignmentgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class kokoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_koko);
+    }
+}
